8f19088ec8f61fc8925928ee1aa00a696f38fbdc
muluk-code <55439119+muluk-code@users.noreply.github.com>
2019-09-21 04:50:16 -0500

    Create estandar.py

---
+++ proyecto_Dorean/libraries/estandar.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#!/home/proydorean/app/entdorean/bin/python3
+import tkinter
+
+coloruno = "DodgerBlue4"
+colordos = "SkyBlue3"
+colortres = "deep sky blue"
+colorcuatro = "DeepSkyBlue2"
+colorcinco = "LightSkyBlue4"
+colorseis = "#33CCFF"
+colorsiete = "DodgerBlue2"
+colorocho = "spring green"
+colornueve = "#00CCFF"
+colordiez = "#00CCCC"
+coloronce = "#00FFCC"
+colordoce = "LightSkyBlue3"
+gris1 = "gray19"
+blanco = "#FFFFFF"
+seccion = "Pri"
+nombre = "Dorean %s" % seccion
+txu = ""
+txuh = ""
